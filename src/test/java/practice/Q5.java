dc399d11afa47f310d100728e8c022cd2ef96996
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-18 20:22:44 +0300

    18.05.22

--- src/test/java/practice/Q5.java
+++ src/test/java/practice/Q5.java
@@ -24,5 +24,48 @@ public class Q5 {
        // close driver
 
         */
+     public static void main(String[] args) throws InterruptedException {
 
+         System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+         WebDriver driver = new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+         // Navigate to website  https://testpages.herokuapp.com/styled/index.html
+         driver.navigate().to("https://testpages.herokuapp.com/styled/index.html");
+
+         // Under the  ORIGINAL CONTENTS
+         // click on Alerts
+         WebElement alert=driver.findElement(By.id("alerts"));
+         alert.click();
+
+         // print the URL
+         String alertUrl= driver.getCurrentUrl();
+         System.out.println("alertUrl = " + alertUrl);
+
+         // navigate back
+         driver.navigate().back();
+
+         // print the URL
+         String homeUrl=driver.getCurrentUrl();
+         System.out.println("homeUrl = " + homeUrl);
+
+         // Click on Basic Ajax
+         WebElement basicAjax=driver.findElement(By.id("basicajax"));
+         basicAjax.click();
+
+         // print the URL
+         String basicAjaxUrl=driver.getCurrentUrl();
+         System.out.println("basicAjaxUrl = " + basicAjaxUrl);
+
+         // enter value-> 20 and Enter
+         Thread.sleep(1500);
+         driver.findElement(By.id("lteq30")).sendKeys("20" + Keys.ENTER);
+
+         // and then verify Submitted Values is displayed open page
+         boolean verify=driver.findElement(By.xpath("//p[text()='Submitted Values']")).isDisplayed();
+         System.out.println( verify ? "Verify test PASS" : "Verify test FAİLED" );
+
+         // close driver
+         driver.close();
+     }
 }
--- src/test/java/practice/Q6.java
+++ src/test/java/practice/Q6.java
@@ -1,5 +1,12 @@
 package practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Q6 {
       /*
     // ...Exercise6...
@@ -13,4 +20,34 @@ public class Q6 {
     9.  Tum sayfalari kapatiniz
 
      */
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //1.  https://id.heroku.com/login sayfasina gidin
+        driver.get("https://id.heroku.com/login");
+        //2.  Bir mail adersi giriniz
+        WebElement emailTextBox=driver.findElement(By.xpath("//input[@class='form-control']"));
+        emailTextBox.sendKeys("abcdfgh@gmail.com");
+        //3.  Bir password giriniz
+        WebElement passwordBox= driver.findElement(By.xpath("//input[@autocomplete='off']"));
+        passwordBox.sendKeys("123456asd");
+        //4.  Login butonuna tiklayiniz
+        WebElement loginButonu=driver.findElement(By.xpath("//button[@name='commit']"));
+        loginButonu.submit();
+        //5.  "There was a problem with your login." texti gorunur ise
+        //6.  "kayit yapilamadi" yazdiriniz
+        //8.  eger yazi gorunur degilse "kayit yapildi" yazdiriniz
+        //9.  Tum sayfalari kapatiniz
+        WebElement gorunenText=driver.findElement(By.xpath("//div[@class='alert alert-danger']"));
+        if (gorunenText.isDisplayed()){
+            System.out.println("kayit yapilamadi");
+        }else{
+            System.out.println("kayit yapildi");
+        }
+        driver.close();
+    }
 }
